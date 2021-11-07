3ac382e584b5816ade607a73e86d9484d816e554
Izhan <babuizhan1234@gmail.com>
2021-11-07 11:26:17 +0530

    added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-button">
+        <h4 className="title">calc</h4>
+        <div className="theme-switch">
+          <p className="theme">THEME</p>
+        </div>
+      </div>
+      <div className="number">{value}</div>
+      <div className="calc-buttons-wrapper">
+        <div className="calc-buttons">
+          <div className="row">
+            <button className="calc">7</button>
+            <button className="calc">8</button>
+            <button className="calc">9</button>
+            <button className="calc del">DEL</button>
+          </div>
+          <div className="row">
+            <button className="calc">4</button>
+            <button className="calc">5</button>
+            <button className="calc">6</button>
+            <button className="calc">+</button>
+          </div>
+          <div className="row">
+            <button className="calc">1</button>
+            <button className="calc">2</button>
+            <button className="calc">3</button>
+            <button className="calc">-</button>
+          </div>
+          <div className="row">
+            <button className="calc">.</button>
+            <button className="calc">0</button>
+            <button className="calc">/</button>
+            <button className="calc">x</button>
+          </div>
+          <div className="row">
+            <button className="calc">RESET</button>
+            <button className="calc">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
