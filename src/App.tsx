bdf088cb25af726b3a4c61193c5f66f8aabe8de0
Izhan <babuizhan1234@gmail.com>
2021-11-07 15:01:42 +0530

    layout complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,27 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState("");
+  const [operator, setOperator] = React.useState("");
+
+  const addNumber = (num: string): void => {
+    setValue("");
+    setValue(value + num);
+  };
+
+  const handleDelete = (): void => {
+    setValue("0");
+    setOperator("");
+  };
+
+  const handleOperator = (str: string): void => {
+    setOperator(str);
+    setValue(value + str);
+  };
+
+  const handleDecimal = () => {
+    setValue(value + ".");
+  };
 
   return (
     <div className="App">
@@ -16,28 +36,60 @@ function App() {
       <div className="calc-buttons-wrapper">
         <div className="calc-buttons">
           <div className="row">
-            <button className="calc">7</button>
-            <button className="calc">8</button>
-            <button className="calc">9</button>
-            <button className="calc del">DEL</button>
+            <button className="calc" onClick={() => addNumber("7")}>
+              7
+            </button>
+            <button className="calc" onClick={() => addNumber("8")}>
+              8
+            </button>
+            <button className="calc" onClick={() => addNumber("9")}>
+              9
+            </button>
+            <button className="calc del" onClick={() => handleDelete()}>
+              DEL
+            </button>
           </div>
           <div className="row">
-            <button className="calc">4</button>
-            <button className="calc">5</button>
-            <button className="calc">6</button>
-            <button className="calc">+</button>
+            <button className="calc" onClick={() => addNumber("4")}>
+              4
+            </button>
+            <button className="calc" onClick={() => addNumber("5")}>
+              5
+            </button>
+            <button className="calc" onClick={() => addNumber("6")}>
+              6
+            </button>
+            <button className="calc" onClick={() => handleOperator("+")}>
+              +
+            </button>
           </div>
           <div className="row">
-            <button className="calc">1</button>
-            <button className="calc">2</button>
-            <button className="calc">3</button>
-            <button className="calc">-</button>
+            <button className="calc" onClick={() => addNumber("1")}>
+              1
+            </button>
+            <button className="calc" onClick={() => addNumber("2")}>
+              2
+            </button>
+            <button className="calc" onClick={() => addNumber("3")}>
+              3
+            </button>
+            <button className="calc" onClick={() => handleOperator("-")}>
+              -
+            </button>
           </div>
           <div className="row">
-            <button className="calc">.</button>
-            <button className="calc">0</button>
-            <button className="calc">/</button>
-            <button className="calc">x</button>
+            <button className="calc" onClick={() => handleDecimal()}>
+              .
+            </button>
+            <button className="calc" onClick={() => addNumber("0")}>
+              0
+            </button>
+            <button className="calc" onClick={() => handleOperator("/")}>
+              /
+            </button>
+            <button className="calc" onClick={() => handleOperator("*")}>
+              x
+            </button>
           </div>
           <div className="row-submit">
             <button className="calc">RESET</button>
